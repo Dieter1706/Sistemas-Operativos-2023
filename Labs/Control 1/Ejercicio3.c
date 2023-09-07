b40b6df7d7ec526f52fc9841b01477a1019123be
Dieter1706 <DieterDDHT@gmail.com>
2023-09-07 11:07:32 -0300

    Ejercicio 3 Terminado C1

--- Labs/Control 1/Ejercicio3.c
+++ Labs/Control 1/Ejercicio3.c
@@ -5,13 +5,26 @@ int main(void){
     int b;
     int c;
 
-    printf("Ingrese el largo del 1° lado del triangulo");
-    scanf("%d", a);
+    printf("Ingrese el largo del 1° lado del triangulo\n");
+    scanf("%d", &a);
 
-    printf("Ingrese el largo del 2° lado del triangulo");
-    scanf("%d", b);
+    printf("Ingrese el largo del 2° lado del triangulo\n");
+    scanf("%d", &b);
     
-    printf("Ingrese el largo del 3° lado del triangulo");
-    scanf("%d", c);
+    printf("Ingrese el largo del 3° lado del triangulo\n");
+    scanf("%d", &c);
 
+    if (a==b){
+        if (a==c){
+            printf("El triangulo es Equilatero");
+        }else{
+            printf("El triangulo es Isosceles");
+        }
+    }else if (a==c){
+        printf("El triangulo es Isosceles");
+    }else if (b==c){
+        printf("El triangulo es Isosceles");
+    }else{
+        printf("El triangulo es Escaleno");
+    } 
 }
